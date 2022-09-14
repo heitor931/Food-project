b0d97e15c2663e274c0c54b4745f56e1f6873c08
Engineer <heitorino1@gmail.com>
2022-09-14 16:08:24 +0100

    added cartReducer

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import "./App.scss";
-import { Fragment } from "react";
 import { useState } from "react";
-
 
 import Header from "./Components/Layout/Header";
 import Meals from "./Components/Meals/Meals";
 import Cart from "./Components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
- 
 function App() {
-
-  const [cartIsShown, setCartIsShown] = useState(false)
+  const [cartIsShown, setCartIsShown] = useState(false);
 
   const showCartHandler = () => {
-    setCartIsShown(true)
-  }
+    setCartIsShown(true);
+  };
 
   const hideCartHandler = () => {
-    setCartIsShown(false)
-  }
+    setCartIsShown(false);
+  };
 
   return (
-    <Fragment>
+    <CartProvider>
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </Fragment>
+    </CartProvider>
   );
 }
 
